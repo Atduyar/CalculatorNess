0637ecc12222c82f53d629d283de6c16a977e8ab
Zafer Çetin <zaferkurumsal@gmail.com>
2024-07-09 20:56:56 +0300

    initial presentation views etc

---
+++ lib/presentation/widgets/button/calculator_rounded_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalculatorRoundedButton extends StatelessWidget {
+  const CalculatorRoundedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
