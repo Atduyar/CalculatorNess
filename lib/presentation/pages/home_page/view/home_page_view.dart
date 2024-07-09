425341c204d0fecb7a5d41e08583c03a6d2e9b95
Zafer Çetin <zaferkurumsal@gmail.com>
2024-07-09 23:25:09 +0300

    ui example presentation

--- lib/presentation/pages/home_page/view/home_page_view.dart
+++ lib/presentation/pages/home_page/view/home_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:calculatorness/presentation/widgets/button/calculator_rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class HomePageView extends StatelessWidget {
@@ -5,6 +6,98 @@ class HomePageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 39, 38, 47),
+      body: _bodySkeleton(),
+    );
+  }
+
+  Widget _bodySkeleton() {
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(flex: 4, child: _calculateScreenPart()),
+          Expanded(flex: 6, child: _inputPart())
+        ],
+      ),
+    );
+  }
+
+  Widget _calculateScreenPart() {
+    return Container(
+      padding: EdgeInsets.only(right: 30),
+      color: Color.fromARGB(255, 39, 38, 47),
+      alignment: Alignment.bottomRight,
+      child: Container(
+        height: 100,
+        alignment: Alignment.bottomRight,
+        child: Text(
+          "1 + 12",
+          style: TextStyle(
+              color: Colors.white, fontSize: 48, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget _inputPart() {
+    return Container(
+      color: const Color.fromARGB(255, 39, 38, 47),
+      padding: EdgeInsets.all(20),
+      child: const Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CalculatorRoundedButton(value: "AC")),
+                Expanded(child: CalculatorRoundedButton(value: "+/-")),
+                Expanded(child: CalculatorRoundedButton(value: "%")),
+                Expanded(child: CalculatorRoundedButton(value: "/")),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CalculatorRoundedButton(value: "7")),
+                Expanded(child: CalculatorRoundedButton(value: "8")),
+                Expanded(child: CalculatorRoundedButton(value: "9")),
+                Expanded(child: CalculatorRoundedButton(value: "*")),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CalculatorRoundedButton(value: "4")),
+                Expanded(child: CalculatorRoundedButton(value: "5")),
+                Expanded(child: CalculatorRoundedButton(value: "6")),
+                Expanded(child: CalculatorRoundedButton(value: "-")),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CalculatorRoundedButton(value: "1")),
+                Expanded(child: CalculatorRoundedButton(value: "2")),
+                Expanded(child: CalculatorRoundedButton(value: "3")),
+                Expanded(child: CalculatorRoundedButton(value: "+")),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(flex: 1, child: CalculatorRoundedButton(value: "S")),
+                Expanded(flex: 1, child: CalculatorRoundedButton(value: "0")),
+                Expanded(flex: 1, child: CalculatorRoundedButton(value: ".")),
+                Expanded(flex: 1, child: CalculatorRoundedButton(value: "=")),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/presentation/widgets/button/calculator_rounded_button.dart
+++ lib/presentation/widgets/button/calculator_rounded_button.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CalculatorRoundedButton extends StatelessWidget {
-  const CalculatorRoundedButton({super.key});
+  final String value;
+  const CalculatorRoundedButton({required this.value, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.all(5),
+      decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 80, 79, 88),
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Center(
+          child: Text(
+        value,
+        style: const TextStyle(color: Colors.white, fontSize: 24),
+      )),
+    );
   }
 }
